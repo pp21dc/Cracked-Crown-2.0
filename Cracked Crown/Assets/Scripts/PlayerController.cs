b295c28eb7b5f86a76896b35365137a152f0eb1c
Ian-Darker <90528854+Ian-Darker@users.noreply.github.com>
2023-10-10 18:34:44 -0400

    added new input system package

---
+++ Cracked Crown/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public void OnMove()
+    {
+        
+    }
+
+    public void OnPrimaryAttack()
+    {
+
+    }
+
+    public void OnExecute()
+    {
+
+    }
+}
