990b18ec21ee77889c932c5ad6062448765e701c
AidWine <aw21pg@brocku.ca>
2023-10-16 16:41:33 -0400

    Update enemy controller

--- Cracked Crown/Assets/Scripts/EnemyScript/EnemyController.cs
+++ Cracked Crown/Assets/Scripts/EnemyScript/EnemyController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
+
 public class EnemyController : MonoBehaviour
 {
     [SerializeField]
@@ -15,14 +16,54 @@ public class EnemyController : MonoBehaviour
 
     private GameObject playerLocator;
 
-    
+
 
     private void Awake()
     {
 
         Players = GameObject.FindGameObjectsWithTag("AddPlayer");
 
-        
+
+        StartCoroutine("findDistance");
 
     }
+
+    IEnumerator findDistance()
+    {
+
+        float check;
+        float currShortest = 10000000000f;
+        GameObject closest;
+    
+       for (int i = 0; i < Players.Length; i++)
+        {
+            
+            check = Vector3.Distance(gameObject.transform.position, Players[i].transform.position);
+
+            if (check < currShortest) 
+            {
+            
+                currShortest = check;
+                closest = Players[i];
+            
+            }
+        
+        }
+
+
+         yield return new WaitForSeconds(5);
+
+        StartCoroutine("findDistance");
+    
+    
+    }
+        
+        
+
+
+
+        
+
+
+
 }
