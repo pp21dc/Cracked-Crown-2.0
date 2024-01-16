753c3b3862f2509ef3d1ec413146a2d52f0a0822
NotACatboy <calvingrypstra@gmail.com>
2024-01-16 14:16:06 -0500

    boss pincer attack working

--- Cracked Crown/Assets/Scenes/Test Scenes/Calvin/Boss scripts/Boss Phases.cs
+++ Cracked Crown/Assets/Scenes/Test Scenes/Calvin/Boss scripts/Boss Phases.cs
@@ -1,69 +1,123 @@
+using UnityEngine;
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
 
 public class BossPhases : MonoBehaviour
 {
     private static float MAXBOSSHEALTH;
+    private static Vector3 LEFTCLAWSPAWN;
 
     [SerializeField] // list for the possible boss attacks
     string[] attackList = new string[3] {"Pincer Attack", "Claw Slam", "Goo Blast"};
     bool attackLoop = true;
+    [Header("Boss Parts")]
+    [SerializeField]
+    GameObject BossObject;
     [SerializeField]
     GameObject clawLeft;
     [SerializeField]
     GameObject clawRight;
+
+    [Header("Boss Variables")]
+    [SerializeField]
+    private float clawspeed;
+    [SerializeField] // count used to allow the boss to know when to initiate attacks
+    private float attacktimer;
+
+    private Vector3 clawtarget;
+    private bool clawfollow = false;
+    private bool clawgrab = false;
+    private bool clawreturn = false;
 
     // gets references for players
     private GameObject[] PlayerList = new GameObject[4];
 
     private float bossHealth;
 
-    [SerializeField]
-    GameObject BossObject;
+    
     private void Awake()
     {
-        GameObject[] TempList = GameObject.FindGameObjectsWithTag("Player");
+        GameObject[] TempList = GameObject.FindGameObjectsWithTag("BossFollowPoint");
         for (int i = 0; i < TempList.Length; i++)
         {
             PlayerList[i] = TempList[i];
-            Debug.Log(PlayerList[i]);
+            Debug.Log(i, PlayerList[i]);
         }
+        LEFTCLAWSPAWN = clawLeft.transform.position;
     }
 
     void Update()
     {
         if (attackLoop)
         {
-            
-            clawLeft.transform.position = PlayerList[0].transform.position + transform.TransformDirection(100,0,0);
             // attacks called in a continuous loop
+            if (attacktimer <= 0)
+            {
+                StartCoroutine("PincerAttack");
+                attacktimer = 30;
+            }
+            if (attacktimer > 22)
+            {
+                pincerAttack();
+            }
+            
+            attacktimer -= Time.deltaTime;
         }
     }
 
-    IEnumerator AttackCycle() // the loop that calls boss attacks throughout the fight
+    IEnumerator AttackCycle() // the loop that calls boss attacks based on attacktimer
     {
         StartCoroutine("PincerAttack");
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(8);
         StartCoroutine("ClawAttack");
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(10);
         StartCoroutine("GooBlast");
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(10);
     }
 
+    private void pincerAttack()
+    {
+        if (clawfollow)
+        {
+            clawtarget = PlayerList[0].transform.position + PlayerList[0].transform.TransformDirection(0, 80, 0);
+            clawLeft.transform.position = Vector3.MoveTowards(clawLeft.transform.position, clawtarget, clawspeed);
+        }
+        if (clawgrab)
+        {
+            clawLeft.transform.position = Vector3.MoveTowards(clawLeft.transform.position, clawtarget, clawspeed*1.5f);
+        }
+        if (clawreturn)
+        {
+            clawLeft.transform.position = Vector3.MoveTowards(clawLeft.transform.position, clawtarget, clawspeed);
+        }
+    }
     IEnumerator PincerAttack()
     {
-        // animation is called
+        clawfollow = true;
+        yield return new WaitForSeconds(4);
+
+        clawfollow = false;
+
+        clawtarget = PlayerList[0].transform.position + PlayerList[0].transform.TransformDirection(0, 40, 0);
+
+        clawgrab = true;
+
         // damage is dealt
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(2);
+        clawgrab = false;
+        clawreturn = true;
+        clawtarget = LEFTCLAWSPAWN;
     }
+
     IEnumerator ClawSweep()
     {
         // animation is called
         // damage is dealt
         yield return new WaitForSeconds(3);
     }
+
     IEnumerator GooBlast()
     {
         // animation is called
