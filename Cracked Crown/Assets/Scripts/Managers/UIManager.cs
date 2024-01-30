31c6a80c4ff599f3d8e201dc6e84413c86e257bd
Patrick Perry <93502969+pp21dc@users.noreply.github.com>
2024-01-30 12:21:03 -0500

    made changes to the art scene and my test scene

--- Cracked Crown/Assets/Scripts/Managers/UIManager.cs
+++ Cracked Crown/Assets/Scripts/Managers/UIManager.cs
@@ -1,67 +1,124 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject InGameUI;
+    [Header("Player Controllers")]
+    [SerializeField] PlayerController player1;
+    [SerializeField] PlayerController player2;
+    [SerializeField] PlayerController player3;
+    [SerializeField] PlayerController player4;
 
-    [SerializeField]
-    private GameObject MainMenu;
+    [Header("Main Menu")]
+    [SerializeField] private GameObject MainMenu;
 
-    [SerializeField]
-    private GameObject PauseMenu;
-    private PauseMenu pauseMenu;
-
-    [SerializeField]
-    private GameObject SettingsMenu;
-
-    [SerializeField]
-    private GameObject CharacterSelectUI;
+    [Header("In Game UI")]
+    [SerializeField] private GameObject InGameUI;
 
 
-    int state = 0;
-    private enum States
+    [Header("Pause Menu")]
+    [SerializeField] private GameObject PauseMenu;
+    [SerializeField] private GameObject pauseMenuInitButton;
+    private bool gamePaused = false;
+
+    [Header("Settings Menu")]
+    [SerializeField] private GameObject SettingsMenu;
+    [SerializeField] Slider MasterSlider;
+    [SerializeField] Slider SFXSlider;
+    [SerializeField] Slider MusicSlider;
+    [SerializeField] TMP_Dropdown Resolution;
+    [SerializeField] private AudioMixer Mixer;
+    [SerializeField] private GameObject settingsInitButton;
+    private bool isFullscreen = true;
+
+    [Header("Character Select")]
+    [SerializeField] private GameObject CharacterSelectUI;
+    private void Update()
     {
-        InGameUI,
-        MainMenu,
-        PauseMenu,
-        SettingsMenu,
-        CharacterSelectUI
-    }
-    [SerializeField]
-    PlayerController controller;
-    // Start is called before the first frame update
-    void Start()
-    {
-        pauseMenu = PauseMenu.GetComponent<PauseMenu>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (controller.PauseDown)
+        if (player1.PauseDown || player2.PauseDown || player3.PauseDown || player4.PauseDown)
         {
-            if (MainMenu.activeSelf)
+            Debug.Log("if 1");
+            if (PauseMenu.activeSelf == false)
             {
-                //Progress to character select
+                Pause();
             }
 
-            if (PauseMenu.activeSelf)
-            {
-                //close pause menu (run "unpause" function in PauseMenu script)
-            }
-
-            if (InGameUI.activeSelf)
-            {
-                //open pause menu (run "pause" menu in PauseMenu script)
-            }
-
-            if (CharacterSelectUI.activeSelf)
-            {
-                //open pause menu (run "pause" menu in PauseMenu script)
-            }
         }
+    }
+
+    //Functions For Pause Menu (Copied from PauseMenu Script 1/30/2024)
+    private void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+
+        gamePaused = true;
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+
+        gamePaused = false;
+    }
+
+    private void closePause()
+    {
+        PauseMenu.SetActive(false);
+    }
+
+    public void OpenSettings()
+    {
+        pauseMenuInitButton = EventSystem.current.currentSelectedGameObject;
+        SettingsMenu.SetActive(true);
+        closePause();
+        EventSystem.current.SetSelectedGameObject(settingsInitButton);
+    }
+
+    public void CloseSettings()
+    {
+        SettingsMenu.SetActive(false);
+        Pause();
+        EventSystem.current.SetSelectedGameObject(pauseMenuInitButton);
+    }
+
+    //Settings Menu
+    public void setResolution()
+    {
+        if (Resolution.value == 0)
+        {
+            Screen.SetResolution(1920, 1080, isFullscreen);
+        }
+        else if (Resolution.value == 1)
+        {
+            Screen.SetResolution(2560, 1440, isFullscreen);
+        }
+        else if (Resolution.value == 2)
+        {
+            Screen.SetResolution(1280, 720, isFullscreen);
+        }
+    }
+
+    public void setMasterVol()
+    {
+        Mixer.SetFloat("MasterVol", MasterSlider.value);
+    }
+
+    public void setSFXVol()
+    {
+        Mixer.SetFloat("SFXVol", SFXSlider.value);
+    }
+
+    public void setMusicVol()
+    {
+        Mixer.SetFloat("MusicVol", MusicSlider.value);
     }
 }
