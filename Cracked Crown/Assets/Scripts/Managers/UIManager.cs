d69b6704b0ef13b614d7d0d34a2508a09b5d1802
Patrick Perry <93502969+pp21dc@users.noreply.github.com>
2024-02-27 12:52:50 -0500

    started re-adding the health bar functionality
    adding health bar functionality to the UI manager
    Accidentally deleted the HealthBar script made a few weeks ago, so making a better designed system that allows the animation of losing health to be integrated

--- Cracked Crown/Assets/Scripts/Managers/UIManager.cs
+++ Cracked Crown/Assets/Scripts/Managers/UIManager.cs
@@ -12,11 +12,21 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [Header("Player Controllers")]
-    [SerializeField] PlayerController player1;
-    [SerializeField] PlayerController player2;
-    [SerializeField] PlayerController player3;
-    [SerializeField] PlayerController player4;
+    [Header("Player Info")]
+    [SerializeField] PlayerController player1Controller;
+    [SerializeField] PlayerController player2Controller;
+    [SerializeField] PlayerController player3Controller;
+    [SerializeField] PlayerController player4Controller;
+
+    [SerializeField] PlayerBody player1Body;
+    [SerializeField] PlayerBody player2Body;
+    [SerializeField] PlayerBody player3Body;
+    [SerializeField] PlayerBody player4Body;
+
+    private float player1LastFrameHealth;
+    private float player2LastFrameHealth;
+    private float player3LastFrameHealth;
+    private float player4LastFrameHealth;
 
     [Header("Main Menu")]
     [SerializeField] private GameObject MainMenu;
@@ -29,7 +39,6 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject player2Ring;
     [SerializeField] private GameObject player3Ring;
     [SerializeField] private GameObject player4Ring;
-
     private float shineTimer = 0f;
     [SerializeField] private float maxRandShineTimer = 20f;
     private float player1ShineTimer = 0f;
@@ -37,6 +46,18 @@ public class UIManager : MonoBehaviour
     private float player3ShineTimer = 0f;
     private float player4ShineTimer = 0f;
 
+    [Header("Health Bars")]
+    [SerializeField] private GameObject player1HealthBarObj;
+    [SerializeField] private GameObject player2HealthBarObj;
+    [SerializeField] private GameObject player3HealthBarObj;
+    [SerializeField] private GameObject player4HealthBarObj;
+
+    private Material player1HealthBar;
+    private Material player2HealthBar;
+    private Material player3HealthBar;
+    private Material player4HealthBar;
+
+    [SerializeField] float animSpeed = 0.5f;
     [Header("Pause Menu")]
     [SerializeField] private GameObject PauseMenu;
     [SerializeField] private GameObject pauseMenuInitButton;
@@ -61,9 +82,18 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Sprite[] CharacterIcons;
     public enum characterIcons {Frog, Bunny, Duck, HoneyBadger };
 
+    private void Start()
+    {
+        RecordPlayerHealths();
+        animSpeed = 1.0f / animSpeed;
+
+
+    }
     private void Update()
     {
-        if (player1.PauseDown || player2.PauseDown || player3.PauseDown || player4.PauseDown)
+        CheckPlayerHealths();
+
+        if (player1Controller.PauseDown || player2Controller.PauseDown || player3Controller.PauseDown || player4Controller.PauseDown)
         {
             Debug.Log("if 1");
             if (PauseMenu.activeSelf == false)
@@ -73,23 +103,7 @@ public class UIManager : MonoBehaviour
         }
 
 
-        if(shineTimer > maxRandShineTimer)
-        {
-            shineTimer = 0;
-            player1ShineTimer = UnityEngine.Random.value * maxRandShineTimer;
-            player2ShineTimer = UnityEngine.Random.value * maxRandShineTimer;
-            player3ShineTimer = UnityEngine.Random.value * maxRandShineTimer;
-            player4ShineTimer = UnityEngine.Random.value * maxRandShineTimer;
-        }
-
-        if (player1ShineTimer == shineTimer)
-        {
-            
-        }
-  /*      if (player)
-        shineTimer += Time.deltaTime;*/
-
-       
+        RecordPlayerHealths();
     }
 
     //Functions For Pause Menu (Copied from PauseMenu Script 1/30/2024)
@@ -109,7 +123,7 @@ public class UIManager : MonoBehaviour
         gamePaused = false;
     }
 
-    private void closePause()
+    private void ClosePause()
     {
         PauseMenu.SetActive(false);
     }
@@ -118,7 +132,7 @@ public class UIManager : MonoBehaviour
     {
         pauseMenuInitButton = EventSystem.current.currentSelectedGameObject;
         SettingsMenu.SetActive(true);
-        closePause();
+        ClosePause();
         EventSystem.current.SetSelectedGameObject(settingsInitButton);
     }
 
@@ -130,7 +144,7 @@ public class UIManager : MonoBehaviour
     }
 
     //Settings Menu
-    public void setResolution()
+    public void SetResolution()
     {
         if (Resolution.value == 0)
         {
@@ -146,26 +160,95 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    public void setMasterVol()
+    public void SetMasterVol()
     {
         Mixer.SetFloat("MasterVol", MasterSlider.value);
     }
 
-    public void setSFXVol()
+    public void SetSFXVol()
     {
         Mixer.SetFloat("SFXVol", SFXSlider.value);
     }
 
-    public void setMusicVol()
+    public void SetMusicVol()
     {
         Mixer.SetFloat("MusicVol", MusicSlider.value);
     }
 
-    public void displayDialogue(string dialogue, characterIcons character)
+    public void DisplayDialogue(string dialogue, characterIcons character)
     {
         DialogueBar.SetText(dialogue);
         CharacterIcon.sprite = CharacterIcons[((int)character)];
         CharacterIcon.SetNativeSize();
         CharacterIcon.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+    }
+
+    private void GetHealthBarMats()
+    {
+        player1HealthBar = player1HealthBarObj.GetComponent<Material>();
+        player2HealthBar = player2HealthBarObj.GetComponent<Material>();
+        player3HealthBar = player3HealthBarObj.GetComponent<Material>();
+        player4HealthBar = player4HealthBarObj.GetComponent<Material>();
+    }
+
+    private void CheckPlayerHealths()
+    {
+        if (CheckPlayer1Health()) { };
+        if (CheckPlayer2Health()) { };
+        if (CheckPlayer3Health()) { };
+        if (CheckPlayer4Health()) { };
+    }
+
+    private bool CheckPlayer1Health()
+    {
+        if(player1LastFrameHealth != player1Body.Health)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool CheckPlayer2Health()
+    {
+        if (player2LastFrameHealth != player2Body.Health)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool CheckPlayer3Health() 
+    {
+        if (player3LastFrameHealth != player3Body.Health)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool CheckPlayer4Health()
+    {
+        if (player4LastFrameHealth != player4Body.Health)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+   /* IEnumerator ModifyHealthBar(Material healthBar, float prevHealth, float currentHealth)
+    {
+        float 
+        healthBar.SetFloat("_Position", Mathf.Lerp(prevHealth, currentHealth, ))
+    }*/
+    private void RecordPlayerHealths()
+    {
+        player1LastFrameHealth = player1Body.Health;
+        player2LastFrameHealth = player2Body.Health;
+        player3LastFrameHealth = player3Body.Health;
+        player4LastFrameHealth = player4Body.Health;
     }
 }
