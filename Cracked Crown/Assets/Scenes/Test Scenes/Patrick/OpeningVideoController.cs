cbcfb9910ad3021e36cd43ebd26faa431e69d7b9
Patrick Perry <patrickperry713@gmail.com>
2024-03-04 16:19:08 -0500

    added opening video

---
+++ Cracked Crown/Assets/Scenes/Test Scenes/Patrick/OpeningVideoController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class OpeningVideoController : MonoBehaviour
+{
+    [SerializeField]
+    VideoClip[] videos = new VideoClip[3];
+
+    [SerializeField]
+    VideoPlayer[] players = new VideoPlayer[3];
+    [SerializeField]
+    VideoPlayer player;
+
+    int j = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < players.Length; i++)
+        {
+            players[i].Prepare();
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Looks to see if a key has been pressed to skip to next video
+        if (Input.anyKeyDown)
+        {
+            players[j].enabled = false;
+            j++;
+            players[j].Play();
+        }
+
+        //checks if first video is finished and starts looping seccond video (showing text with anim)
+        if (player.isPaused)
+        {
+            if(j == 0)
+            {
+                players[j].enabled = false;
+
+                j++;
+
+                players[j].Play();
+                players[j].isLooping = true;
+            }
+        }
+
+        
+    }
+}
