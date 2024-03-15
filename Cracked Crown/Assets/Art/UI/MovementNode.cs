fd5442a059e77abf59fe61cef3ec13022f7b5515
Patrick Perry <93502969+pp21dc@users.noreply.github.com>
2024-03-15 13:28:26 -0400

    updated blob effect
    Still needs custom node written to move blobs around randomly

---
+++ Cracked Crown/Assets/Art/UI/MovementNode.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.ShaderGraph;
+using System.Reflection;
+
+public class MovementNode : MonoBehaviour
+{
+
+  
+}
