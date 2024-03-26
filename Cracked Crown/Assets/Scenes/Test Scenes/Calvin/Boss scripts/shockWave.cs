80c0742ef4565fb54f4618ea7f434840baeda79a
NotACatboy <calvingrypstra@gmail.com>
2024-03-26 02:38:56 -0400

    Huge Boss chamhes

--- Cracked Crown/Assets/Scenes/Test Scenes/Calvin/Boss scripts/shockWave.cs
+++ Cracked Crown/Assets/Scenes/Test Scenes/Calvin/Boss scripts/shockWave.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class shockWave : MonoBehaviour
 {
+    private GameObject[] playerList = new GameObject[4];
     void Awake()
     {
         StartCoroutine("Death");
@@ -14,5 +15,28 @@ public class shockWave : MonoBehaviour
     {
         yield return new WaitForSeconds(1.25f);
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (playerList[i] != null)
+            {
+                if (playerList[i] == other.gameObject)
+                {
+                    return;
+                } 
+            }
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerBody playerScript = other.gameObject.GetComponent<PlayerBody>();
+            if (!playerScript.dashing)
+            {
+                playerScript.DecHealth(3);
+            }
+
+        }
     }
 }
