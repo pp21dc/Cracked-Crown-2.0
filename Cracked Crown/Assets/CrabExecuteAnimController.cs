9eae0592c6cde20d18b6bb5bb2bdef0df8bf20c7
Ian-Darker <90528854+Ian-Darker@users.noreply.github.com>
2024-04-05 11:41:09 -0400

    nto done yet

---
+++ Cracked Crown/Assets/CrabExecuteAnimController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabExecuteAnimController : MonoBehaviour
+{
+    [SerializeField]
+    private CrabWalk miniCrab;
+
+    private void ResetBunny()
+    {
+
+    }
+    private void ResetFrog()
+    {
+
+    }
+    private void ResetDuck()
+    {
+
+    }
+    private void ResetBadger()
+    {
+
+    }
+
+}
